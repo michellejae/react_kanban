18a19c6635af47cf7a3b64832ca50d5ebf6726dd
michelle moyer <moyer.mj@gmail.com>
2018-02-24 09:44:17 -1000

    completed put

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -4,13 +4,34 @@ const Card = require(`../models/Card`)
 
 
 router
+//.delete(`/:id`)
+
+.put(`/:id`, (req, res) => {
+  let id = req.params.id
+  let {priority, name, status, created_by, assigned_to} = req.body
+  return new Card({id, name, priority, status, created_by, assigned_to})
+  .save()
+  .then(updatedCard => {
+    updatedCard = updatedCard.toJSON();
+    res.json(updatedCard)
+  })
+})
+
+.get(`/:id`, (req, res) => {
+  return new Card({id: req.params.id})
+  .fetch()
+  .then(card => {
+    card = card.toJSON();
+    res.json(card)
+  })
+})
 
 .get(`/`, (req, res) => {
   return Card
   .fetchAll()
-  .then(result => {
-    result = result.toJSON();
-    res.json(result)
+  .then(cards => {
+    cards = cards.toJSON();
+    res.json(cards)
   })
 
 })
@@ -19,9 +40,9 @@ router
   let {name, priority, status, created_by, assigned_to} = req.body
   return new Card({name, priority, status, created_by, assigned_to})
   .save()
-  .then(result => {
-    result = result.toJSON();
-    res.json(result)
+  .then(newCard => {
+    newCard = newCard.toJSON();
+    res.json(newCard)
   })
 })
 
