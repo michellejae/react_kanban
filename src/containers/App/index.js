00a83d3d7e00e4cdb135cd506b94847e2f278e23
michelle moyer <moyer.mj@gmail.com>
2018-07-22 21:52:28 -0400

    background image

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -29,6 +29,7 @@ class App extends Component {
 
   render() {
     return (
+      <div className="main">
       <div className="parentClass">
         <div className="EditCard">
             {this.props.showEdit ? <EditCardForm id={this.props.showEdit}/> : null}
@@ -41,6 +42,7 @@ class App extends Component {
           <button className="formButton" onClick={this.handleNewCardForm}> New Task </button>
           <ColumnListContainer />
         </div>
+      </div>
       </div>
     )
   }
